cd501f3f69bdbe222b83fdcd01557b0acf8dadbd
Mohamed Akram <am@razrlab.com>
2020-10-26 16:16:02 +0400

    Add codec 13

---
+++ test/codec13.test.js
@@ -0,0 +1,29 @@
+/** @typedef {import("zora").SpecFunction} SpecFunction */
+
+const { codec } = require("..");
+
+function print(obj) {
+  console.log(JSON.stringify(obj, null, 2));
+}
+
+/** @type {SpecFunction} */
+function test1(t) {
+  // https://wiki.teltonika-gps.com/view/Codec#Codec_13
+  const hex = "00000000000000130D01050000000B0A81C320676574696E666F010000ED9B";
+  const actual = codec.decode(Buffer.from(hex, "hex"));
+  const expected = {
+    preamble: 0,
+    dataFieldLength: 19,
+    codecId: 13,
+    commandQuantity1: 1,
+    type: 5,
+    commandSize: 11,
+    timestamp: 176276256,
+    command: "getinfo",
+    commandQuantity2: 1,
+    crc16: 60827,
+  };
+  t.deepEqual(actual, expected, "codec 13 is parsed correctly");
+}
+
+module.exports = test1;
