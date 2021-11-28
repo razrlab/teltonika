328f3ee44abfcc60102317047505efba30cef1f7
Mohamed Akram <am@razrlab.com>
2021-11-28 23:48:57 +0400

    Improve types

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/** @typedef {import('./lib/avl').AVL} AVL */
 module.exports.avl = require("./lib/avl");
 module.exports.codec = require("./lib/codec");
 module.exports.Decoder = require("./lib/decoder");
